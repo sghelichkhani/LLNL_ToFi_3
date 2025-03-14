bc46e36457b3c58399dc971b23087b31074f93f2
Sia Ghelichkhan <ghelichkhani.siavash@gmail.com>
2025-03-14 14:22:18 +1100

    Adding a linter

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ Date:     2024-08-12
 
 LLNL_ToFi
 
-Example routines for determining the values of a seismic velocity model on the 
+Example routines for determining the values of a seismic velocity model on the
 grid points of the LLNL-G3D-JPS model.
 
     Original work Copyright (C) 2019 Bernhard Schuberth (bernhard.schuberth@lmu.de)
@@ -29,16 +29,12 @@ grid points of the LLNL-G3D-JPS model.
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    
+
 """
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 from mpi4py import MPI
 import numpy as np
-from pathlib import Path
-import sys
-
-import ctypes as C
 
 
 from utils import (R_EARTH_KM, LLNL_PATH, LLNL_COORD_FILE, LLNL_DEPTH_FILE,
@@ -48,7 +44,7 @@ from utils import (R_EARTH_KM, LLNL_PATH, LLNL_COORD_FILE, LLNL_DEPTH_FILE,
 import utils
 
 
-#--------------------------------------------------------------------------    
+# --------------------------------------------------------------------------
 def init_model_parallel(comm=0):
 
     myrank = comm.Get_rank()
@@ -58,15 +54,17 @@ def init_model_parallel(comm=0):
 
     model = []
     if myrank == 0:
-        model = read_model() 
+        model = read_model()
 
     model = comm.bcast(model, root=0)
 
     comm.barrier()
 
     return model
-  
-#--------------------------------------------------------------------------    
+
+# --------------------------------------------------------------------------
+
+
 def read_model():
 
     # USER MODIFICATION REQUIRED
@@ -76,20 +74,22 @@ def read_model():
 
     return model
 
-#--------------------------------------------------------------------------    
-def project_slowness_3D(model,radius_avg,lat,lon,radius_min,radius_max,grid_spacing):
+# --------------------------------------------------------------------------
+
+
+def project_slowness_3D(model, radius_avg, lat, lon, radius_min, radius_max, grid_spacing):
 
     # This is a dummy routine that needs to be modified by the user.
 
-    # Please modify the code to obtain 3-D slowness perturbations (i.e., the absolute difference between 
+    # Please modify the code to obtain 3-D slowness perturbations (i.e., the absolute difference between
     # 3-D and 1-D slowness at the current point) for your model.
     # Depending on your model parametrization (i.e., coarser or finer than LLNL-G3D-JPS), you will need
-    # to either perform an interpolation (e.g., from the nearest neighbor's on your grid to the current 
+    # to either perform an interpolation (e.g., from the nearest neighbor's on your grid to the current
     # point in the LLNL-G3D-JPS grid if coarser), or you will have to compute an average value in the
     # volume given by "radius_min" and "radius_max" in vertical direction and grid_spacing as
     # search radius in lateral direction (e.g., by an inverse-distance weighting algorithm).
 
-    # NOTE: Perturbation in slowness is du = (1/v_3D - 1/v_1D), and du is approximately -(v - v_1D)/(v_1D**2) 
+    # NOTE: Perturbation in slowness is du = (1/v_3D - 1/v_1D), and du is approximately -(v - v_1D)/(v_1D**2)
     #       => du = -dv/v_1D^2 = -dln(v)/v_1D
 
     # USER MODIFICATION REQUIRED
@@ -98,13 +98,14 @@ def project_slowness_3D(model,radius_avg,lat,lon,radius_min,radius_max,grid_spac
 
     return du
 
-#--------------------------------------------------------------------------    
-def model_1D(model,radius):
+# --------------------------------------------------------------------------
+
+
+def model_1D(model, radius):
 
     # This is a dummy routine that needs to be modified by the user
 
     # Please modify the code to obtain the 1-D seismic velocity value for the given radius.
-
 
     # USER MODIFICATION REQUIRED
     v_1D = []
@@ -112,20 +113,21 @@ def model_1D(model,radius):
 
     return v_1D
 
-#--------------------------------------------------------------------------    
-def get_slowness_layer(model,radius_in,lat,lon,grid_spacing):
+# --------------------------------------------------------------------------
 
-    # This is a dummy routine that illustrates how to get values of a seismic velocity 
-    # model in terms of slowness perturbation du = 1/v_3D - 1/v_1D onto the grid 
-    # of the LLNL-G3D-JPS tomographic model. 
+
+def get_slowness_layer(model, radius_in, lat, lon, grid_spacing):
+
+    # This is a dummy routine that illustrates how to get values of a seismic velocity
+    # model in terms of slowness perturbation du = 1/v_3D - 1/v_1D onto the grid
+    # of the LLNL-G3D-JPS tomographic model.
     # Note: dv = -du*v_1D^2 => dv/v_1D = dln(v) = -du*v_1D; du = -dln(v)/v_1D
-
 
     # USER MODIFICATION REQUIRED
     # This routine expects radius to be given in km.
     # Thus, normalize the radii if necessary (uncomment the line below if applicable).
-    #r_norm = R_EARTH_KM
-    r_norm = 1. # no radius normalization by default
+    # r_norm = R_EARTH_KM
+    r_norm = 1.  # no radius normalization by default
     # END USER MODIFICATION REQUIRED
 
     # turn input radius into a vector if not already provided in this form
@@ -139,22 +141,22 @@ def get_slowness_layer(model,radius_in,lat,lon,grid_spacing):
         radius_max = radius_in["max"] / r_norm
 
     # Get 1-D seismic velocity for that layer
-    v_1D = model_1D(model,radius_avg[0])
+    v_1D = model_1D(model, radius_avg[0])
 
     slowness_perturbation = np.ones(len(lat))
     # Loop over all points
     for ip in np.arange(len(lat)):
-            
-        # Get 3-D (absolute) slowness perturbation du = 1/v_3D - 1/v_1D
-        slowness_perturbation[ip]  = project_slowness_3D(model,radius_avg[ip],lat[ip],lon[ip],radius_min[ip],radius_max[ip],grid_spacing)
 
+        # Get 3-D (absolute) slowness perturbation du = 1/v_3D - 1/v_1D
+        slowness_perturbation[ip] = project_slowness_3D(
+            model, radius_avg[ip], lat[ip], lon[ip], radius_min[ip], radius_max[ip], grid_spacing)
 
     return slowness_perturbation, v_1D
 
 
-#--------------------------------------------------------------------------    
-def reparam(comm,radii,gc_lat,lon,reparam):
-    
+# --------------------------------------------------------------------------
+def reparam(comm, radii, gc_lat, lon, reparam):
+
     myrank = comm.Get_rank()
     num_procs = comm.Get_size()
 
@@ -162,7 +164,7 @@ def reparam(comm,radii,gc_lat,lon,reparam):
     nl = len(radii)
 
     slowness_perturbation = {}
-    
+
     if reparam:
         # USER MODIFICATION REQUIRED
         # Initialize the seismic model (if necessary)
@@ -171,8 +173,7 @@ def reparam(comm,radii,gc_lat,lon,reparam):
 
     v_1D = np.zeros(nl)
 
-    for ilyr in range(1,nl+1):
-
+    for ilyr in range(1, nl + 1):
 
         # Initialize model vectors for that layer
         if ilyr <= nl_UM_TZ:
@@ -184,11 +185,10 @@ def reparam(comm,radii,gc_lat,lon,reparam):
             # nominal grid spacing is 2 degree in the lower mantle
             grid_spacing = 222.
 
-        
-        slowness_perturbation[ilyr-1] = np.zeros(cnp,dtype='float64')
+        slowness_perturbation[ilyr - 1] = np.zeros(cnp, dtype='float64')
 
         if reparam:
-    
+
             if myrank == 0:
                 if ilyr == 1:
                     print('#')
@@ -199,33 +199,33 @@ def reparam(comm,radii,gc_lat,lon,reparam):
                 else:
                     print('#       ... layer %2d ...' % ilyr)
 
-
             # Distribute work load on all processors
-            [ cnp_sub, my_ib, my_ie ] = utils.parallelize(myrank,num_procs,cnp)
+            [cnp_sub, my_ib, my_ie] = utils.parallelize(myrank, num_procs, cnp)
 
-            m_true = np.zeros(cnp,dtype='float64')
-            tmp = np.zeros(cnp,dtype='float64')
-        
+            m_true = np.zeros(cnp, dtype='float64')
+            tmp = np.zeros(cnp, dtype='float64')
 
             # Get slowness and 1-D velocity at current location
-            [  tmp[my_ib:my_ie], v_1D_tmp ] = get_slowness_layer(model,radii[ilyr-1], gc_lat[my_ib:my_ie],lon[my_ib:my_ie],
-                                                                            grid_spacing)
+            [tmp[my_ib:my_ie], v_1D_tmp] = get_slowness_layer(model, radii[ilyr - 1], gc_lat[my_ib:my_ie], lon[my_ib:my_ie],
+                                                              grid_spacing)
 
-            v_1D[ilyr-1] = v_1D_tmp[0]
-        
-            comm.Allreduce([tmp, MPI.DOUBLE], [slowness_perturbation[ilyr-1], MPI.DOUBLE], op = MPI.SUM)
+            v_1D[ilyr - 1] = v_1D_tmp[0]
+
+            comm.Allreduce([tmp, MPI.DOUBLE], [
+                           slowness_perturbation[ilyr - 1], MPI.DOUBLE], op=MPI.SUM)
 
             if myrank == 0:
                 # Note: dv = -du*v_1D^2 => dv/v_1D = dln(v) = -du*v_1D
                 # reparametrised model (dln(v))
-                m_true = -1. * slowness_perturbation[ilyr-1] * v_1D[ilyr-1]
+                m_true = -1. * slowness_perturbation[ilyr - 1] * v_1D[ilyr - 1]
 
                 # Output reparametrised model
-                header = '# v1D: %12.7f ' % v_1D[ilyr-1]
-                utils.write_layer(ilyr,m_true,radii[ilyr-1]["avg"],lon,gc_lat,OUTFILE_PARM_PREFIX,string=header)
-    
+                header = '# v1D: %12.7f ' % v_1D[ilyr - 1]
+                utils.write_layer(
+                    ilyr, m_true, radii[ilyr - 1]["avg"], lon, gc_lat, OUTFILE_PARM_PREFIX, string=header)
+
         else:
-    
+
             if myrank == 0:
                 if ilyr == 1:
                     print('#')
@@ -238,20 +238,18 @@ def reparam(comm,radii,gc_lat,lon,reparam):
 
             m_true = []
             header = ''
-            if myrank == 0: 
+            if myrank == 0:
                 # reparametrised model
-                [lon_in, gc_lat_in, m_true, header] = utils.read_layer(ilyr,radii[ilyr-1]["avg"],OUTFILE_PARM_PREFIX)
+                [lon_in, gc_lat_in, m_true, header] = utils.read_layer(
+                    ilyr, radii[ilyr - 1]["avg"], OUTFILE_PARM_PREFIX)
 
             m_true = comm.bcast(m_true, root=0)
             header = comm.bcast(header, root=0)
 
-            v_1D[ilyr-1] = header[-1]
-    
+            v_1D[ilyr - 1] = header[-1]
+
             # Convert velocity to slowness perturbation du
             # dv = -du*v_1D^2 => dv/v_1D = dln(v) = -du*v_1D, du = -dln(v)/v_1D
-            slowness_perturbation[ilyr-1] = -1. * m_true / v_1D[ilyr-1] 
-
+            slowness_perturbation[ilyr - 1] = -1. * m_true / v_1D[ilyr - 1]
 
     return slowness_perturbation, v_1D
-
-
